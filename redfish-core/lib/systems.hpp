1ab5d20bbd97114774bf239a3d74b4c5965959c8
zeuspan <zeuspan@tencent.com>
2021-08-05 12:52:24 +0000

    develop, product A

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -32,12 +32,17 @@
 #include "zdb.h"
 #include <ctime>
 #include <uuid/uuid.h>
+#include <openssl/md5.h>
 
 namespace redfish
 {
 /**
  * Systems derived class for delivering Computer Systems Schema.
  */
+    static size_t writeFunction(void* ptr, size_t size, size_t nmemb, std::string* data) {
+        data->append( static_cast<char*>(ptr), size * nmemb);
+        return size * nmemb;
+    }
 class Systems : public Node
 {
   public:
@@ -67,10 +72,6 @@ class Systems : public Node
         res.end();
     }
 
-    static size_t writeFunction(void* ptr, size_t size, size_t nmemb, std::string* data) {
-        data->append( static_cast<char*>(ptr), size * nmemb);
-        return size * nmemb;
-    }
 
     void doPost(crow::Response& response, const crow::Request& req,
                const std::vector<std::string>&) override
@@ -147,7 +148,7 @@ class Systems : public Node
 
         if (response.jsonValue["chargeStatus"] == 1)
         {
-            std::cout<<" begin to insert db:"<< "name:"<<name<<" idNum:"<<idNum<<"  cardNo:"<<cardNo<<"  mobile:"<<mobile <<std::endl;
+            //std::cout<<" begin to insert db:"<< "name:"<<name<<" idNum:"<<idNum<<"  cardNo:"<<cardNo<<"  mobile:"<<mobile <<std::endl;
             zdb::Connection conn = crow::dbconnections::dbpoll->getConnection();
             zdb::PreparedStatement p1 = conn.prepareStatement("insert into runoob_tbl (name, idNum, cardNo, mobile) values(?, ?, ?, ?);");
             conn.beginTransaction();
@@ -202,66 +203,160 @@ class SystemResetActionInfo : public Node
         res.jsonValue["Name"] = "Anti-Fraud Product";
         res.end();
     }
+    static std::string getUrl(std::string carNo, std::string mobile, std::string name, std::string userIp)
+    {
+        std::string reqUrl("https://antielectricfraud-prod-api.qunlicloud.com/api/nsc/openapi/multipleLoans?");
+        reqUrl += "phoneNumber=";
+        reqUrl += mobile;
+        reqUrl += "&";
+        reqUrl += "idNumber=";
+        reqUrl += carNo;
+        if (name != "")
+        {
+            reqUrl += "&name:";
+            reqUrl += name;
+        }
+        if (userIp != "")
+        {
+            reqUrl += "&userIp:";
+            reqUrl += userIp;
+        }
+        BMCWEB_LOG_DEBUG<<"product A, getUrl:"<<reqUrl;
+        return reqUrl;
+    }
+    static struct curl_slist * getHeaderList(std::string)
+    {
+        struct curl_slist *chunk = NULL;
+        std::time_t t = std::time(0);
+        uuid_t  nonce;
+        uuid_generate_random(&nonce);
+        std::string nonceStr(nonce);
+        std::string clientId("172409b6-f7df-11ea-be6e-fa163efee3db");
 
+        std::string hdrTimeStamp("Ql-Auth-Timestamp: ");
+        std::string hdrNonce("Ql-Auth-Nonce: ");
+        std::string hdrSign("Ql-Auth-Sign: ");
+        std::string hdrClientId("Ql-Auth-clientId: ");
+        /* calculate MD5 stage one : MD5(nonce+timestamp) */
+        unsigned char md1[16];
+        unsigned char md2[16];
+        unsigned char md5input[100];
+        memset(md5input, 0, sizeof(md5input));
+        memcpy(md5input, nonce, sizeof(uuid_t));
+        memcpy(md5input + sizeof(uuid_t), &t, sizeof(std::time_t));
+
+        MD5(md5input, sizeof(uuid_t) + sizeof(std::time_t), md1);
+        /* calculate MD5 stage two : MD5(MD5(nonce+timestamp)+clientId) */
+        memset(md5input, 0, sizeof(md5input));
+        memcpy(md5input, md1, sizeof(md1));
+        strcpy(md5input, clientId.c_str());
+
+        MD5(md5input, sizeof(md1) + clientId.length(), md2);
+        //std::string sign(md2);
+
+        hdrTimeStamp += std::to_string(t);
+        hdrNonce += nonceStr;
+        hdrSign.append(sign);
+        hdrClientId += clientId;
+
+        //chunk = curl_slist_append(chunk, "Accept:");
+
+        chunk = curl_slist_append(chunk, hdrTimeStamp.c_str());
+
+        chunk = curl_slist_append(chunk, hdrNonce.c_str());
+
+        chunk = curl_slist_append(chunk, hdrSign.c_str());
+
+        chunk = curl_slist_append(chunk, hdrClientId.c_str());
+
+        return chunk;
+    }
     void doPost(crow::Response& response, const crow::Request& req,
                const std::vector<std::string>&) override
     {
         std::string cardNo;
         std::string mobile;
         std::optional<std::string> name("");
+        std::optional<std::string> userIp("");
         if (!json_util::readJson(
-                req, response, "cardNo",cardNo, "mobile", mobile, "name", name))
+                req, response, "cardNo",cardNo, "mobile", mobile, "name", name, "userIp", userIp))
         {
             return;
         }
-        uuid_t  nonce;
 
-        std::time_t t = std::time(0);
-        uuid_generate_random(&nonce);
-        std::string clientId("172409b6-f7df-11ea-be6e-fa163efee3db");
+        std::string reqUrl = getUrl(cardNo, mobile, *name, *userIp);
+        std::string response_string;
+        std::string header_string;
 
-        std::string hdrTimeStamp("Ql-Auth-Timestamp: ");
-        std::string hdrNonce("Ql-Auth-Nonce: ");
-        std::string hdrSign("Ql-Auth-Sign: ");
-        std::string hdrClientId("Ql-Auth-ClientId: ");
 
-        hdrTimeStamp += std::to_string(t);
-        hdrNonce += std::to_string(nonce);
-        hdrSign += 
+        //BMCWEB_LOG_DEBUG << "timestamp:"<<t;
+/*         for (i = 0; i < 16; i++){
+            sprintf(tmp,"%2.2x",md[i]);
+            strcat(buf,tmp);
+        }
+        printf("%s/n",buf); */
 
-        BMCWEB_LOG_DEBUG << "timestamp:"<<t;
 
         CURL *curl;
-        CURLcode res;
-        
+
         curl = curl_easy_init();
         if(curl) {
-            struct curl_slist *chunk = NULL;
-        
-            /* Remove a header curl would otherwise add by itself */
-            chunk = curl_slist_append(chunk, "Accept:");
-        
-            /* Add a custom header */
-            chunk = curl_slist_append(chunk, "Q1-Auth-Timestamp: yes");
-        
-            /* Modify a header curl otherwise adds differently */
-            chunk = curl_slist_append(chunk, "Host: example.com");
-        
-            /* Add a header with "blank" contents to the right of the colon. Note that
-            we're then using a semicolon in the string we pass to curl! */
-            chunk = curl_slist_append(chunk, "X-silly-header;");
-        
+            struct curl_slist *chunk = getHeaderList();
+
+/*             chunk = curl_slist_append(chunk, "Accept:");
+            chunk = curl_slist_append(chunk, hdrTimeStamp.c_str());
+            chunk = curl_slist_append(chunk, hdrNonce.c_str());
+            chunk = curl_slist_append(chunk, hdrSign.c_str());
+
+            chunk = curl_slist_append(chunk, hdrClientId.c_str()); */
+
             /* set our custom set of headers */
             curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-        
-            curl_easy_setopt(curl, CURLOPT_URL, "localhost");
+
             curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
             //curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 1024L);
-            curl_easy_setopt(hnd, CURLOPT_URL, "https://antielectricfraud-prod-api.qunlicloud.com/api/nsc/openapi/multipleLoans");
-            curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
-            curl_easy_setopt(hnd, )
+            curl_easy_setopt(curl, CURLOPT_URL, reqUrl.c_str());
+            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
 
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
 
+            curl_easy_perform(curl);
+
+            curl_easy_cleanup(curl);
+            curl = NULL;
+            curl_slist_free_all(chunk);
+            curl_global_cleanup();
+
+            response.jsonValue = nlohmann::json::parse(response_string);
+
+            std::cout<<"code: "<<response.jsonValue["code"] <<std::endl;
+
+            if (response.jsonValue["code"] == "10000")
+            {
+                std::cout<<" begin to insert db for product A:"<<"  cardNo:"<<cardNo<<"  mobile:"<<mobile <<std::endl;
+/*                 zdb::Connection conn = crow::dbconnections::dbpoll->getConnection();
+                zdb::PreparedStatement p1 = conn.prepareStatement("insert into runoob_tbl (name, idNum, cardNo, mobile) values(?, ?, ?, ?);");
+                conn.beginTransaction();
+                p1.bind(1, name);
+                p1.bind(2, idNum);
+                p1.bind(3, cardNo); // include terminating \0
+                p1.bind(4, mobile);
+                p1.execute();
+                conn.commit(); */
+            }
+            else
+            {
+                std::cout<<" query failed:"<<response.jsonValue["code"]<<std::endl;
+            }
+            response.end();
+
+        }
+        else
+        {
+            messages::serviceInUnknownState(response);
+            return;
         }
     }
 
